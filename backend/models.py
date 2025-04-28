92cc9dfd7bb9366bbc89d8926949f9099f40b5a5
charlesbarnabas <charlesrickybarnabas@gmail.com>
2025-04-28 16:37:41 +0700

    update: delete data menu

--- backend/models.py
+++ backend/models.py
@@ -46,7 +46,7 @@ class OrderItem(db.Model):
     __tablename__ = 'order_items'
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
-    menu_id = db.Column(db.Integer, db.ForeignKey('menus.id'), nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menus.id', ondelete='CASCADE'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
     order = db.relationship('Order', backref=db.backref('order_items', lazy=True))
